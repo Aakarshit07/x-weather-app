41dbb58c139654393a933007a4b62d69da62b218
Aakarshit07 <aakarshit070@gmail.com>
2024-03-29 18:43:55 +0530

    CSS Classes fixed for weather card

--- src/WeatherCard.jsx
+++ src/WeatherCard.jsx
@@ -3,20 +3,20 @@ function WeatherCard({ data }) {
     // console.log("props data", data);
     const {current , location} = data;
     return (
-        <div className="weather-cards">
-            <div className="weather-card">
+        <div className="weather-card">
+            <div className="weather-cards">
                 <h1>Temperature</h1>
                 <p>{current?.temp_c}°C</p>
             </div>
-            <div className="weather-card">
+            <div className="weather-cards">
                 <h1>Humidity</h1>
                 <p>{current?.humidity}%</p>
             </div>
-            <div className="weather-card">
+            <div className="weather-cards">
                 <h1>Condition</h1>
                 <p>{current?.condition?.text}</p>
             </div>
-            <div className="weather-card"> 
+            <div className="weather-cards"> 
                 <h1>Wind Speed</h1>
                 <p>{current?.wind_kph}kph</p>
             </div>
